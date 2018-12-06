69eb5bee99f062fc571aa79e5b53c28a99ab62b2
Jessica <jessicaperezvalle@pursuit.org>
2018-12-05 21:03:04 -0500

    Design Format

--- app/src/main/java/org/pursuit/group_portfolio_hw_codigo/MainActivity.java
+++ app/src/main/java/org/pursuit/group_portfolio_hw_codigo/MainActivity.java
@@ -1,7 +1,10 @@
 package org.pursuit.group_portfolio_hw_codigo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View main = findViewById(R.id.main);
+        main.setBackgroundResource(R.drawable.background_opt_1);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, JessActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+
+
+
     }
 }

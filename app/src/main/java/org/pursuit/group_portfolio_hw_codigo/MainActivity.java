74fa55286b07255bd61c812c3427d762d75b8968
Geo Jimenez <ger.jimenez1@gmail.com>
2018-12-07 23:54:29 -0500

    write nav-drawer/toolbar logic

--- app/src/main/java/org/pursuit/group_portfolio_hw_codigo/MainActivity.java
+++ app/src/main/java/org/pursuit/group_portfolio_hw_codigo/MainActivity.java
@@ -2,8 +2,13 @@ package org.pursuit.group_portfolio_hw_codigo;
 
 import android.content.Intent;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.graphics.drawable.Drawable;
@@ -15,22 +20,36 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+    private android.support.v7.widget.Toolbar toolBar;
     private NavigationView navView;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        drawerLayout = findViewById(R.id.main);
+        toolBar = findViewById(R.id.my_toolbar);
         navView = findViewById(R.id.nav_view);
+        setSupportActionBar(toolBar);
+
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_nav_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()) {
                     case R.id.option_Geo:
-                        Intent intent = new Intent(MainActivity.this, GeoActivity.class);
-                        startActivity(intent);
+                        Intent intentGeo = new Intent(MainActivity.this, GeoActivity.class);
+                        startActivity(intentGeo);
+                        break;
+                    case R.id.option_Jessica:
+                        Intent intentJess = new Intent(MainActivity.this, JessActivity.class);
+                        startActivity(intentJess);
                         break;
                     default:
                         Toast.makeText(MainActivity.this, "Feature Not Yet Implemented", Toast.LENGTH_SHORT).show();
@@ -39,5 +58,25 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(this, "Open The Drawer!", Toast.LENGTH_SHORT).show();
+                drawerLayout.openDrawer(Gravity.START, true);
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
